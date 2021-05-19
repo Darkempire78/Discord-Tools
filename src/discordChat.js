effd12a3c4e40467c1bedc685837e5ec3f7114ed
Darkempire78 <clement.wawszczyk@gmail.com>
2021-05-19 21:22:56 +0200

    remove urlify

--- src/discordChat.js
+++ src/discordChat.js
@@ -83,14 +83,6 @@ function convertMessageContent(message) {
 
 }
 
-function urlify(text) {
-    var urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, function(url) {
-        return `<a href="${url}">${url}</a>`;
-    })
-}
-
-
 // Exports
 module.exports = {
     getDiscordChatWebviewContent,
