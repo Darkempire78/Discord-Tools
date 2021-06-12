5538cb3246cfb80508b0f9bfc6952484ef19490b
Laaru <60797172+Laar-u@users.noreply.github.com>
2021-06-12 11:30:27 +0300

    this looks better imo

--- src/pyTemplate/Cogs/onCommandError.py
+++ src/pyTemplate/Cogs/onCommandError.py
@@ -15,13 +15,13 @@ class OnCommandErrorCog(commands.Cog, name="on command error"):
 			hour = round(error.retry_after/3600)
 			minute = round(error.retry_after/60)
 			if day > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+str(day)+ "day(s)")
+				await ctx.send('This command has a cooldown, for '+str(day)+ "day(s)")
 			elif hour > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+str(hour)+ " hour(s)")
+				await ctx.send('This command has a cooldown, for '+str(hour)+ " hour(s)")
 			elif minute > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+ str(minute)+" minute(s)")
+				await ctx.send('This command has a cooldown, for '+ str(minute)+" minute(s)")
 			else:
-				await ctx.send(f'This command has a cooldown, be sure to wait for {error.retry_after:.2f} second(s)')
+				await ctx.send(f'This command has a cooldown, for {error.retry_after:.2f} second(s)')
 		elif isinstance(error, CommandNotFound):
 			return
 		elif isinstance(error, MissingPermissions):
