b00706ef17c6013670fad7f23082198d91e1d3b0
Tanish <tanishazad03@gmail.com>
2022-01-29 19:53:20 +0530

    update JDA Template

--- src/jdaTemplate/src/main/java/com/example/artifact_id/BotCommand.java
+++
@@ -1,8 +0,0 @@
-package com.example.artifact_id;
-
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
-public interface BotCommand {
-	void run(GuildMessageReceivedEvent event,String[] args);
-	String getHelp();
-}
